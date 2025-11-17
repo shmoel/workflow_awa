2cdbdc4f622a2232f9942c1ff8a3d5390446468f
SRE <sreshmoel@gmail.com>
2025-11-17 14:53:30 +0100

    version 6 : tentative 15 de resolution upload, et problème annexes

--- backend/routes/route.py
+++ backend/routes/route.py
@@ -72,10 +72,22 @@ async def download_file(request: Request, filename: str):
 
     if not os.path.exists(file_path):
         raise HTTPException(status_code=404, detail=f"Fichier non trouvé : {filename_only}")
+    
+    # Détection simple du type
+    ext = filename_only.split(".")[-1].lower()
+
+    media_types = {
+        "pdf": "application/pdf",
+        "jpg": "image/jpeg",
+        "jpeg": "image/jpeg",
+        "png": "image/png"
+    }
+
+    media_type = media_types.get(ext, "application/octet-stream")
 
     return FileResponse(
         path=file_path,
-        media_type="application/octet-stream",
+        media_type=media_type,
         filename=filename_only,
         headers={"Content-Disposition": f'inline; filename*=UTF-8\'\'{filename_only}'}
     )
